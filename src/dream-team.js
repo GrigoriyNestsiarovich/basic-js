a19c59a250f19857401f391d227736b2a4271cb1
GrigoriyNestsiarovich <79170403+GrigoriyNestsiarovich@users.noreply.github.com>
2021-03-07 00:14:53 +0300

    feat: add code

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+     let g = '';
+    var subArr = [];
+    const member = (['Olivia', 1111, 'Lily', 'Oscar', true, null, 'Stas']);
+    for (var i = 0; i < member.length; i++)
+    {
+        if(typeof member[i] == 'string')
+        {
+            g = member[i].slice("");
+            subArr.push(g[0]);
+        }
+    }
+    if (g == ''){
+        return false;
+}
+return subArr.sort().join('').toUpperCase();
 };
